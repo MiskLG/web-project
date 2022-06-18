5f4159a47439313747a2308d1050a500a80df0aa
MiskLG <hornjak.ra139.2019@uns.ac.rs>
2022-06-18 15:09:59 +0200

    [FIX] Fixed bad commit

--- web/src/main/java/beans/Manager.java
+++ web/src/main/java/beans/Manager.java
@@ -1,5 +1,22 @@
 package beans;
 
+import java.util.Date;
+
 public class Manager extends User{
 	private SportsFacility facility;
+
+	public Manager(String username, String name, String lastname, String password, GenderType gender, Date dateOfBirth,
+			UserType userType, SportsFacility facility) {
+		super(username, name, lastname, password, gender, dateOfBirth, userType);
+		this.facility = facility;
+	}
+
+	public SportsFacility getFacility() {
+		return facility;
+	}
+
+	public void setFacility(SportsFacility facility) {
+		this.facility = facility;
+	}
+	
 }
