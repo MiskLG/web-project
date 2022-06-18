f2f4164214e61e305fe56f812253096e115b00dc
MiskLG <hornjak.ra139.2019@uns.ac.rs>
2022-06-18 15:06:49 +0200

    [NEW] Added visited facility tracker to buyer

--- web/src/main/java/beans/Buyer.java
+++ web/src/main/java/beans/Buyer.java
@@ -1,16 +1,20 @@
 package beans;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Buyer extends User{
 	private Subscription subscriptionId;
+	private ArrayList<SportsFacility> visitedFacilities;
 	private int points;
 	private BuyerType buyerType;
-	
+		
 	public Buyer(String username, String name, String lastname, String password, GenderType gender, Date dateOfBirth,
-			UserType userType, Subscription subscriptionId, int points, BuyerType buyerType) {
+			UserType userType, Subscription subscriptionId, ArrayList<SportsFacility> visitedFacilities, int points,
+			BuyerType buyerType) {
 		super(username, name, lastname, password, gender, dateOfBirth, userType);
 		this.subscriptionId = subscriptionId;
+		this.visitedFacilities = visitedFacilities;
 		this.points = points;
 		this.buyerType = buyerType;
 	}
@@ -32,6 +36,12 @@ public class Buyer extends User{
 	}
 	public void setBuyerType(BuyerType buyerType) {
 		this.buyerType = buyerType;
+	}
+	public ArrayList<SportsFacility> getVisitedFacilities() {
+		return visitedFacilities;
+	}
+	public void setVisitedFacilities(ArrayList<SportsFacility> visitedFacilities) {
+		this.visitedFacilities = visitedFacilities;
 	}
 	
 	
