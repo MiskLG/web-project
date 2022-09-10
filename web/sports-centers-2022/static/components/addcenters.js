76af70f7aaaddccc2e8c09181e1173f94f5e5015
MiskLG <hornjak.ra139.2019@uns.ac.rs>
2022-09-10 12:32:02 +0200

    [NEW] Added a map to choose location from

--- web/sports-centers-2022/static/components/addcenters.js
+++ web/sports-centers-2022/static/components/addcenters.js
@@ -4,7 +4,9 @@ Vue.component("add-centers", {
 	    	user: {username:"", type:""},
 	    	credentials: {username: "", password: ""},
 
+            mapClicked : false,
             managers: "",
+            map: "",
             center: {name: "",type: "",latitude: "",longitude: "",city: "",
                     street: "", stNumber: "", poNumber: "", image: "",
                      startTime: "", endTime: "", managerUsername: ""} 
@@ -96,6 +98,9 @@ Vue.component("add-centers", {
     </nav>
         <form>
                 <div class="bg-secondary rounded text-light row mx-4 mt-2 border border-primary">
+                    <div class="row m-2 d-flex justify-content-center">
+                        <div id="map-create" style="height:500px;width:1200px"></div>   
+                    </div>
                     <div class="row m-2">
                         <div class="col-md-2 pt-1">
                             <label class="form-label col-md-6">Name:</label>
@@ -195,7 +200,8 @@ Vue.component("add-centers", {
                                 </option>
                             </select>
                          </div>
-                    </div>   
+                    </div>
+                   
                     <div class="d-flex justify-content-center align-center row m-1">
 					<button @click="addFacility" type="button" class="btn btn-primary">
 						Add Facility
@@ -272,6 +278,7 @@ Vue.component("add-centers", {
                                 router.push("/");
                                 window.location.reload();
                             }
+                            this.displayMap();
 						}
                         else{
                             router.push("/");
@@ -377,10 +384,78 @@ Vue.component("add-centers", {
                     })
                     .catch(error => {
                     })
+            },
+            displayMap: function () {
+
+                let lat = 45.2396;
+                let lon = 19.8227;
+                let map = new ol.Map({
+                    layers: [
+                        new ol.layer.Tile({
+                            source: new ol.source.OSM()
+                        })
+                    ],
+                    view: new ol.View({
+                        center: ol.proj.fromLonLat([lon, lat]),
+                        zoom: 10
+                    })
+                });
+    
+                setTimeout(() => {
+                    if (map) {
+                        map.setTarget("map-create");
+                        let c = document.getElementById("map-create").childNodes;
+                        c[0].style.borderRadius = '15px';
+                    }
+                }, 50);
+    
+                map.on('click', evt => {
+                    let coord = ol.proj.toLonLat(evt.coordinate);
+                    this.reverseGeocode(coord);
+                    this.mapClicked = true;
+                })
+            },
+            reverseGeocode: function (coords) {
+                fetch('http://nominatim.openstreetmap.org/reverse?format=json&lon=' + coords[0] + '&lat=' + coords[1])
+                    .then(function (response) {
+                        return response.json();
+                    }).then(json => {
+                    this.center.longitude = coords[0];
+                    this.center.latitude = coords[1];
+                    if (json.address.city_district) {
+                        this.center.city = json.address.city_district;
+                    }
+                    else if (json.address.city) {
+                        this.center.city = json.address.city;
+                    } 
+    
+                    if (json.address.road) {
+                        this.center.street = json.address.road;
+                    }
+    
+                    if (json.address.house_number) {
+                        this.center.stNumber = json.address.house_number;
+                    }
+    
+                    if (json.address.postcode) {
+                        this.center.poNumber = json.address.postcode;
+                    }
+    
+                });
+            },
+            geocode: async function (street, city) {
+                await fetch('http://nominatim.openstreetmap.org/search?format=json&street=' + street + '&city=' + city + '&country=srbija')
+                    .then(function (response) {
+                        return response.json();
+                    }).then(json => {
+                        this.center.longitude = json[0].lon;
+                        this.center.latitude = json[0].lat;
+                    });
             }
     	},
     	mounted () {
             this.getUser();
             this.getManagers();
+            
         }
 });
